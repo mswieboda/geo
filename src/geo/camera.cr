d0ef1fe5c6a2e9f352dc0ba0248b7cbc509a809e
Matt Swieboda <mswiebod@gmail.com>
2019-05-16 22:02:36 -0500

    rotate target

--- src/geo/camera.cr
+++ src/geo/camera.cr
@@ -1,6 +1,7 @@
 module Geo
   class Camera
     getter target : LibRay::Vector3
+    getter rotation_angle : LibC::Float
 
     def initialize
       @position = LibRay::Vector3.new(x: 10, y: 20, z: 20)
@@ -8,8 +9,7 @@ module Geo
       @up = LibRay::Vector3.new(x: 0, y: 1, z: 0)
       @fovy = 45
       @camera = LibRay::Camera.new
-      @mouse_x = 0
-      @mouse_y = 0
+      @rotation_angle = 0
     end
 
     def update
@@ -17,6 +17,10 @@ module Geo
 
       z_delta = 0
       x_delta = 0
+      r_delta = 0
+
+      r_delta = -1 if LibRay.key_down?(LibRay::KEY_Q)
+      r_delta = 1 if LibRay.key_down?(LibRay::KEY_E)
 
       z_delta = -1 if LibRay.key_down?(LibRay::KEY_UP) || LibRay.key_down?(LibRay::KEY_W)
       z_delta = 1 if LibRay.key_down?(LibRay::KEY_DOWN) || LibRay.key_down?(LibRay::KEY_S)
@@ -24,31 +28,15 @@ module Geo
       x_delta = -1 if LibRay.key_down?(LibRay::KEY_LEFT) || LibRay.key_down?(LibRay::KEY_A)
       x_delta = 1 if LibRay.key_down?(LibRay::KEY_RIGHT) || LibRay.key_down?(LibRay::KEY_D)
 
+      @rotation_angle += -100 * r_delta * frame_time if r_delta != 0
+
+      puts @rotation_angle
+
       @target.z += 10 * z_delta * frame_time if z_delta != 0
       @target.x += 10 * x_delta * frame_time if x_delta != 0
 
       @position.z += 10 * z_delta * frame_time if z_delta != 0
       @position.x += 10 * x_delta * frame_time if x_delta != 0
-
-      x_delta = 0
-      y_delta = 0
-
-      mouse_x = LibRay.get_mouse_x
-      mouse_y = LibRay.get_mouse_y
-
-      x_delta = -1 if mouse_x < @mouse_x
-      x_delta = 1 if mouse_x > @mouse_x
-
-      y_delta = -1 if mouse_y < @mouse_y
-      y_delta = 1 if mouse_y > @mouse_y
-
-      @target.x += 50 * x_delta * frame_time if x_delta != 0
-      @target.y += -10 * y_delta * frame_time if y_delta != 0
-
-      @position.y += 10 * y_delta * frame_time if y_delta != 0
-
-      @mouse_x = mouse_x
-      @mouse_y = mouse_y
 
       update_camera
     end
--- src/geo/world.cr
+++ src/geo/world.cr
@@ -2,6 +2,10 @@ module Geo
   class World
     def initialize
       @camera = Camera.new
+
+      # Note: length is spelled wrong in bindings:
+      target_mesh = LibRay.gen_mesh_cube(width: 1, height: 1, lenght: 1)
+      @target_model = LibRay.load_model_from_mesh(target_mesh)
     end
 
     def update
@@ -13,12 +17,13 @@ module Geo
 
       LibRay.draw_grid(100, 0.5)
 
-      LibRay.draw_cube(
+      LibRay.draw_model_ex(
+        model: @target_model,
         position: @camera.target,
-        width: 1,
-        height: 1,
-        length: 1,
-        color: LibRay::GREEN
+        rotation_axis: LibRay::Vector3.new(x: 0, y: 1, z: 0),
+        rotation_angle: @camera.rotation_angle,
+        scale: LibRay::Vector3.new(x: 3, y: 1, z: 1),
+        tint: LibRay::GREEN
       )
 
       LibRay.end_3d_mode
