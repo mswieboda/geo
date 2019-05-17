c4edb160cd62fd390091a8599bd219660b995a80
Matt Swieboda <mswiebod@gmail.com>
2019-05-16 20:23:24 -0500

    rotate camera for testing

--- src/geo/world.cr
+++ src/geo/world.cr
@@ -2,18 +2,23 @@ module Geo
   class World
     def initialize
       @camera = LibRay::Camera.new(
-        position: LibRay::Vector3.new(x: 0, y: 10, z: 10),
+        position: LibRay::Vector3.new(x: 10, y: 20, z: 20),
         target: LibRay::Vector3.new(x: 0, y: 0, z: 0),
         up: LibRay::Vector3.new(x: 0, y: 1, z: 0),
-        fovy: 45
+        fovy: 45.0
       )
+
+      LibRay.set_camera_mode(@camera, 2)
     end
 
     def update
+      LibRay.update_camera(pointerof(@camera))
     end
 
     def draw
       LibRay.begin_3d_mode(@camera)
+
+      LibRay.draw_grid(100, 0.5)
 
       LibRay.draw_cube(
         position: LibRay::Vector3.new(x: 0, y: 0, z: 0),
